08da86ef0abba9c247d94f41792c1f624d61b168
Pearce Keesling <keeslinp@gmail.com>
2024-09-25 19:11:59 -0700

    Inject timezone

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -51,6 +51,8 @@ kotlin {
     sourceSets {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+            implementation(project.dependencies.platform(libs.koin.bom))
+            implementation(libs.koin.test)
         }
         commonMain.dependencies {
             implementation(libs.androidx.room.runtime)
