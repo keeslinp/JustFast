1706b7c59ad7e073f966c6a61fb6e625449566be
Pearce Keesling <keeslinp@gmail.com>
2024-09-26 12:24:44 -0700

    Enable edge-to-edge

--- composeApp/src/androidMain/kotlin/org/keeslinp/fasting/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/keeslinp/fasting/MainActivity.kt
@@ -3,6 +3,7 @@ package org.keeslinp.fasting
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -11,6 +12,7 @@ import org.keeslinp.fasting.screens.DefaultRootComponent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         val root = DefaultRootComponent(componentContext = defaultComponentContext())
