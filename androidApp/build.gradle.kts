b5e5da647abe93b3e45265ec65b8de3062f778a8
Pearce Keesling <keeslinp@gmail.com>
2024-09-26 20:04:39 -0700

    Add timezone mock to new test

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.kotlinx.datetime)
 
     testImplementation(libs.junit)
+    testImplementation(libs.koin.test)
     testImplementation(libs.robolectric)
     testImplementation(libs.androidx.test.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- androidApp/src/test/java/org/keeslinp/fasting/FastRowTest.kt
+++ androidApp/src/test/java/org/keeslinp/fasting/FastRowTest.kt
@@ -8,19 +8,31 @@ import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import kotlinx.datetime.TimeZone
 import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.keeslinp.fasting.data.fast.DisplayFast
 import org.keeslinp.fasting.ui.FastRow
 import org.keeslinp.fasting.ui.theme.JustFastTheme
+import org.koin.core.context.loadKoinModules
 import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.KoinTest
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
-class FastRowTest {
+class FastRowTest: KoinTest {
     @get:Rule val composeTestRule = createComposeRule()
+
+    @Before
+    fun injectTimeZone() {
+        loadKoinModules(module {
+           single<TimeZone> { TimeZone.of("America/Phoenix") }
+       })
+    }
 
     @After
     fun cleanup() {
